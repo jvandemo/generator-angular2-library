46dec719cb9ae188a748fac272b102b0247fa67f
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-12-06 16:54:18 +0100

    feat(generator): add generator logic

--- generators/app/index.js
+++ generators/app/index.js
@@ -12,29 +12,128 @@ module.exports = yeoman.generators.Base.extend({
       'Welcome to the ' + chalk.red('Angular 2 Library') + ' generator!'
     ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    var prompts = [
+      {
+        type: 'input',
+        name: 'authorName',
+        message: 'Your full name:',
+        validate: function (input) {
+          if (/.+/.test(input)) {
+            return true;
+          }
+          return 'Please enter your full name';
+        },
+        default: this.user.git.name
+      },
+      {
+        type: 'input',
+        name: 'authorEmail',
+        message: 'Your email address:',
+        validate: function (input) {
+          if (/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(input)) {
+            return true;
+          }
+          return 'Please enter a valid email address';
+        },
+        default: this.user.git.email
+      },
+      {
+        type: 'input',
+        name: 'libraryName',
+        message: 'Your library name (kebab case)',
+        default: 'angular2-library',
+        store: true
+      },
+      {
+        type: 'input',
+        name: 'gitRepositoryUrl',
+        message: 'Git repository url',
+        default: 'https://github.com/username/repo',
+        store: true
+      }
+    ];
 
     this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
+
+      this.props = {
+
+        author: {
+          name: props.authorName,
+          email: props.authorEmail
+        },
+
+        libraryName: {
+          original: props.libraryName,
+          kebabCase: props.libraryName
+        },
+
+        gitRepositoryUrl: props.gitRepositoryUrl
+      };
 
       done();
     }.bind(this));
+
   },
 
-  writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+  writing: {
+
+    copyGitIgnore: function copyGitIgnore() {
+      this.fs.copy(
+        this.templatePath('gitignore'),
+        this.destinationPath('.gitignore')
+      );
+    },
+
+    copyNpmIgnore: function copyNpmIgnore() {
+      this.fs.copy(
+        this.templatePath('npmignore'),
+        this.destinationPath('.npmignore')
+      );
+    },
+
+    copyTravisYml: function copyTravisYml() {
+      this.fs.copy(
+        this.templatePath('travis.yml'),
+        this.destinationPath('.travis.yml')
+      );
+    },
+
+    copySrc: function copySrc() {
+      this.fs.copy(
+        this.templatePath('src/**/*'),
+        this.destinationPath('src')
+      );
+    },
+
+    copyTypeScriptConfig: function copyTypeScriptConfig() {
+      this.fs.copyTpl(
+        this.templatePath('_tsconfig.json'),
+        this.destinationPath('tsconfig.json'),
+        {
+          props: this.props
+        }
+      );
+    },
+
+    copyPackageJson: function copyPackageJson() {
+      this.fs.copyTpl(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json'),
+        {
+          props: this.props
+        }
+      );
+    },
+
+    copyMain: function copyMain() {
+      this.fs.copy(
+        this.templatePath('main.ts'),
+        this.destinationPath(this.props.libraryName.kebabCase + '.ts')
+      );
+    }
   },
 
   install: function () {
-    this.installDependencies();
+    this.installDependencies({bower: false});
   }
 });
