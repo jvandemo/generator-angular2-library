2fb6465b9b7b28003635885d2c9d2922e6a173e6
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-12-06 16:51:55 +0100

    test(generator): add generator tests

--- test/app.js
+++ test/app.js
@@ -4,16 +4,66 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-generator').test;
 
 describe('generator-angular-2-library:app', function () {
+
   before(function (done) {
+
     helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({someOption: true})
-      .withPrompts({someAnswer: true})
+      .withOptions({})
+      .withPrompts({libraryName: 'angular2-library-name'})
       .on('end', done);
+
   });
 
-  it('creates files', function () {
+  it('should create package.json', function () {
     assert.file([
-      'dummyfile.txt'
+      'package.json'
     ]);
   });
+
+  it('should create tsconfig.json', function () {
+    assert.file([
+      'tsconfig.json'
+    ]);
+  });
+
+  it('should create .gitignore', function () {
+    assert.file([
+      '.gitignore'
+    ]);
+  });
+
+  it('should create .npmignore', function () {
+    assert.file([
+      '.npmignore'
+    ]);
+  });
+
+  it('should create main library file', function () {
+    assert.file([
+      'angular2-library-name.ts'
+    ]);
+  });
+
+  it('should create sample pipes', function () {
+    assert.file([
+      'src/pipes.ts',
+      'src/pipes/sample.pipe.ts'
+    ]);
+  });
+
+  it('should create sample services', function () {
+    assert.file([
+      'src/services.ts',
+      'src/services/sample.service.ts'
+    ]);
+  });
+
+  it('should create sample directives', function () {
+    assert.file([
+      'src/directives.ts',
+      'src/directives/sample.directive.ts',
+      'src/directives/sample.component.ts'
+    ]);
+  });
+
 });
