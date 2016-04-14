fe704b86f148c105d3c64ba1ec9189ca7a9c619a
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2016-04-14 08:24:08 +0200

    feat(tsconfig): add browser.d.ts to files instead of tripleslash

--- generators/app/templates/main.ts
+++ generators/app/templates/main.ts
@@ -1,5 +1,3 @@
-/// <reference path="node_modules/angular2/typings/browser.d.ts" />
-
 import {DIRECTIVES} from './src/directives';
 import {PIPES} from './src/pipes';
 import {PROVIDERS} from './src/services';
