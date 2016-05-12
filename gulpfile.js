6c01e19fde0efab5c848ad75fae7860b19752582
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2016-05-12 08:03:53 +0200

    feat(gulp): increase test timeout to prevent build fails

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ gulp.task('test', ['pre-test'], function (cb) {
 
   gulp.src('test/**/*.js')
     .pipe(plumber())
-    .pipe(mocha({reporter: 'spec', timeout: 5000}))
+    .pipe(mocha({reporter: 'spec', timeout: 20000}))
     .on('error', function (err) {
       mochaErr = err;
     })
