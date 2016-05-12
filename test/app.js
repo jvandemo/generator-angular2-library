1fd4d3153f79d2ac75ed1fdfc8db50e01a987f92
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2016-05-12 07:48:07 +0200

    feat(yeoman): update deprecated items

--- test/app.js
+++ test/app.js
@@ -1,7 +1,7 @@
 'use strict';
 var path = require('path');
 var assert = require('yeoman-assert');
-var helpers = require('yeoman-generator').test;
+var helpers = require('yeoman-test');
 
 describe('generator-angular-2-library:app', function () {
 
