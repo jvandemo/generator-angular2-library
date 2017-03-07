9b62b64facf2dd8aa9a066874fd2009c339453cd
Carlos Roso <ce.roso398@gmail.com>
2017-03-07 07:41:18 -0500

    fix(templates): add spaces before/after imports (#49)
    * chore: update codelyzer and tslint dependencies
    
    * refactor: add spaces before/after curly braces in imports

--- generators/app/templates/src/sample.directive.ts
+++ generators/app/templates/src/sample.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef} from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[sampleDirective]'
--- generators/app/templates/src/sample.pipe.ts
+++ generators/app/templates/src/sample.pipe.ts
@@ -1,4 +1,4 @@
-import {Injectable, PipeTransform, Pipe} from '@angular/core';
+import { Injectable, PipeTransform, Pipe } from '@angular/core';
 
 /**
  * Transforms any input value
